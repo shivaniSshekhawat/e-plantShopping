c9989b0c0f1e12e6c0c8b3197f3ca380da0e5d7c
shivaniSshekhawat <shivanishekhawat2694@gmail.com>
2025-10-16 17:47:27 +0530

    *update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "shoppingreact",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/shoppingreact/" : "/",
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
